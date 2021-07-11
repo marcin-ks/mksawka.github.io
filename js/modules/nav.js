64b78e3cec75a4ba415f67c669f2c79676ef1dcb
mksawka <marcin.k.sawka@gmail.com>
2021-07-11 22:10:48 +0200

    Improved sass, minor js change

--- js/modules/nav.js
+++ js/modules/nav.js
@@ -3,9 +3,9 @@
 export function initModule() {
 
 	const links = document.querySelectorAll('.menu > a');
+	const menu = document.querySelector('.menu');
 
 	function toggleMobileMenu() {
-		const menu = document.querySelector('.menu');
 		const menuBtn = document.querySelector('.menu__mobile-menu-btn');
 
 		menuBtn.addEventListener('click', function() {
@@ -13,7 +13,7 @@ export function initModule() {
 		});
 	}
 
-	function handleMenuLink() {
+	function setClickedLinkAsActive() {
 		for (const link of links) {
 			link.addEventListener('click', function(event) {
 
@@ -27,20 +27,32 @@ export function initModule() {
 	function doSmoothScroll() {
 		for (const link of links) {
 			link.addEventListener('click', function(event) {
-				
 				event.preventDefault();
 				const sectionHref = this.getAttribute("href");
 
-				document.querySelector(sectionHref).scrollIntoView({
-   			behavior: "smooth"
-  			});
+				if (this.parentElement.classList.contains('show')) {
+					setTimeout(function() {
+						menu.classList.remove('show');
+					}, 900);
+
+					setTimeout(function() {
+						document.querySelector(sectionHref).scrollIntoView({
+						behavior: "smooth"
+						});
+					}, 1400);
+				}
+				else {
+					document.querySelector(sectionHref).scrollIntoView({
+					behavior: "smooth"
+					});
+				}
 
 			});
 		}	
 	}
 
 	toggleMobileMenu();
-	handleMenuLink();
+	setClickedLinkAsActive();
 	doSmoothScroll();
 
 }
